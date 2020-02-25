1d7b48879fcb9191690e5f4cdf8d4113f843c3de
GuavaX <57407388+GuavaX@users.noreply.github.com>
2020-02-25 21:01:37 +0800

    apk

---
+++ UnityProject/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("玩家資料")]
+    public PlayerData dataPlayer;
+
+    public void BuyHp_500()
+    {
+        dataPlayer.hpMax += 500;
+        dataPlayer.hp = dataPlayer.hpMax;
+    }
+
+    public void BuyAtk_50()
+    {
+        dataPlayer.attack += 50;
+    }
+
+    public void LoadLevel()
+    {
+        dataPlayer.hp = dataPlayer.hpMax;
+        SceneManager.LoadScene("關卡 1");
+    }
+
+}
